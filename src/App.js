f3f3fc691731c528ce21bf82220829b18eaf3a9d
Sameony <iam2001sam@gmail.com>
2022-12-09 12:34:36 +0530

    feat: Routing and templating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { LandingPage, LoginSignup, Products } from './Routes';
 function App() {
   return (
-    <div className="App">
-      da
-    </div>
+      <Router>
+        <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/auth" element={<LoginSignup />}></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+      </Router>
   );
 }
 
---
+++ src/Routes/index.js
@@ -0,0 +1,5 @@
+import LandingPage from "./LandingPage";
+import Products from "./Products";
+import LoginSignup from "./LoginSignup";
+
+export {LandingPage, Products, LoginSignup}
