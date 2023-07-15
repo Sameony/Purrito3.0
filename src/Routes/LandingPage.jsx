f95f2e61d895df3713ebd2bd00169515899bf1bc
Sameony <iam2001sam@gmail.com>
2023-07-15 23:30:49 +0530

    feat: stock images and carousel on landing page

--- src/Routes/LandingPage.jsx
+++ src/Routes/LandingPage.jsx
@@ -1,147 +1,65 @@
 import React, { useState, useEffect } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { useNavigate } from "react-router-dom";
-function LandingPage() {
-  const navigate = useNavigate();
-  const [offset, setOffset] = useState(0);
-  const [activerewards, setActiveRewards] = useState(true);
+function LandingPage({deviceType}) {
+  // const navigate = useNavigate();
+  deviceType="desktop"
   useEffect(() => {
-    if(!sessionStorage.getItem("username"))
-    {
-      navigate("/auth")
-    }
-    const onScroll = () => setOffset(window.pageYOffset);
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
+    // if(!sessionStorage.getItem("username"))
+    // {
+    //   navigate("/auth")
+    // }
   }, []);
-  const rewards = [
-    {
-      reward:"5% off on next visit",
-      cost:"10 Points"
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
     },
-    {
-      reward:"flat 15$ off on next visit",
-      cost:"25 Points"
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
     },
-    {
-      reward:"50% off on teeth whitenings",
-      cost:"350 Points"
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
     },
-    {
-      reward:"Amazon Gift card worth 35$",
-      cost:"500 Points"
-    },
-    {
-      reward:"Free teeth cleaning",
-      cost:"700 Points"
-    }
-
-  ]
-  const History = [
-    {
-      reward:"5% off on next visit",
-      cost:"10 Points",
-      claimed:"13 Feb 2022"
-    },
-    {
-      reward:"Amazon Gift card worth 35$",
-      cost:"500 Points",
-      claimed:"27 Aug 2022"
-    },
-
-  ]
+  };
   return (
-    <div style={{ minWidth: "100vw" }}>
-      <div id="square" contentEditable />
-      {/*clip css */}
-      <div className="gridContainer">
-        <div className="child1">
-          <div style={{ fontSize: "1.8rem", padding: "10px" }}>
-            Introducting{" "}
-            <span style={{ color: "var(--primary)", fontWeight: "900" }}>
-              Weave Rewardz!
-            </span>
-          </div>
-          <div style={{ fontSize: "1.3rem" }}>
-            So far, your loyalty has accumulated 300 Points!
-          </div>
-        </div>
-        <div className="child2"></div>
-        <div className="child3">
-          <div
-            style={{
-              padding: "1rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              className={activerewards ? "rewardTab selected" : "rewardTab"}
-              onClick={() => {
-                setActiveRewards(true);
-              }}
-            >
-              Rewards
-            </div>
-          </div>
-          <div
-            style={{
-              padding: "1rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              className={!activerewards ? "rewardTab selected" : "rewardTab"}
-              onClick={() => {
-                setActiveRewards(false);
-              }}
-            >
-              My rewards
-            </div>
-          </div>
-          {activerewards ? (
-            <div className="boxContent">
-              <table className="boxTable">
-                <tr className="boxHead">
-                  <th style={{width:"50%"}}>Reward</th>
-                  <th style={{width:"20%"}}>Cost</th>
-                  <th style={{width:"20%"}}>{""}</th>
-                </tr>
-                {rewards.map((ele,index)=>{
-                  return <tr key={index}>
-                    <td style={{width:"50%"}}>{ele.reward}</td>
-                    <td style={{width:"20%"}}>{ele.cost}</td>
-                    <td className="redeemTable">Redeem</td>
-                  </tr>
-                })}
-              </table>
-            </div>
-          ) : (
-            <div className="boxContent">
-              <table className="boxTable">
-                <tr className="boxHead">
-                  <th style={{width:"50%"}}>Reward</th>
-                  <th style={{width:"20%"}}>Cost</th>
-                  <th style={{width:"20%"}}>Claimed on</th>
-                </tr>
-                {History.map((ele,index)=>{
-                  return <tr key={index}>
-                    <td style={{width:"50%"}}>{ele.reward}</td>
-                    <td style={{width:"20%"}}>{ele.cost}</td>
-                    <td style={{width:"20%"}}>{ele.claimed}</td>
-                  </tr>
-                })}
-              </table>
-            </div>
-          )}
-
-        </div>
+    <Carousel
+      swipeable={true}
+      draggable={false}
+      showDots={true}
+      responsive={responsive}
+      infinite={true}
+      autoPlay={deviceType !== "mobile" ? true : false}
+      autoPlaySpeed={2000}
+      keyBoardControl={true}
+      customTransition="all .5"
+      transitionDuration={500}
+      containerClass="carousel-container"
+      removeArrowOnDeviceType={["tablet", "mobile"]}
+      deviceType={deviceType}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+      itemAriaLabel="as"
+    >
+      <div className="bannerImgWrapper">
+        <img className="imageFit" src="images/kyotComfy.jpg" alt="cat" />
       </div>
-    </div>
+      <div className="bannerImgWrapper">
+        <img className="imageFit" src="images/kyotAsleep.jpg" alt="cat" />
+      </div>
+      <div className="bannerImgWrapper">
+        <img className="imageFit" src="images/kyotAsleep2.jpg" alt="cat" />
+      </div>
+      <div className="bannerImgWrapper">
+        <img className="imageFit" src="images/kyotPeek.jpg" alt="cat" />
+      </div>
+    </Carousel>
   );
 }
 
